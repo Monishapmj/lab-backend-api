e5de0efd8e7c6acf7dc0bc0a1bcc42bb7f3f291c
monisha2k4 <137487461+monisha2k4@users.noreply.github.com>
2025-08-08 14:46:18 +0530

    Added server.js, models, and routes for CRUD API

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://MonishaJ:<Ajiabimoni123>@cluster0.8idyd6n.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+// Routes
+const itemRoutes = require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+
+// Start server
+app.listen(5000, () => console.log("Server running on port 5000"));
